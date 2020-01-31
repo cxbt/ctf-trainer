eec9486d53957172fb35e3eb80ba0a63646b27cc
Jehwan Yun <cluxebot@gmail.com>
2020-01-31 09:27:35 +0900

    feat: Implement flag auth routine

--- flaskr/chall.py
+++ flaskr/chall.py
@@ -2,7 +2,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.auth import login_required, admin_required
 from flaskr.db import get_db
@@ -22,98 +22,37 @@ def index():
     return render_template('chall/index.html', challenges=challenges)
 
 
-@bp.route('/create', methods=('GET', 'POST'))
 @login_required
-@admin_required
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        flag = request.form['flag']
-        score = request.form['score']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        try:
-            int(score)
-        except ValueError:
-            error = 'Score should be numeric.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO challenge (title, body, thumbsup, flag, score)'
-                ' VALUES (?, ?, ?, ?, ?)',
-                (title, body, 0, generate_password_hash(flag), score)
-            )
-            db.commit()
-            return redirect(url_for('chall.index'))
-
-    return render_template('chall/create.html')
-
-
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
-@login_required
-@admin_required
-def update(id):
-    challenge = get_challenge(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        score = request.form['score']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        try:
-            int(score)
-        except ValueError:
-            error = 'Score should be numeric.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE challenge SET title = ?, body = ?, score = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('chall.index'))
-
-    return render_template('chall/update.html', challenge=challenge)
-
-
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
-@admin_required
-def delete(id):
-    get_challenge(id)
+@bp.route('/challenge/<int:id>', methods=('POST',))
+def authenticate(id):
     db = get_db()
-    db.execute('DELETE FROM challenge WHERE id = ?', (id,))
-    db.commit()
-    return redirect(url_for('chall.index'))
-
-
-def get_challenge(id, check_admin=True):
-    challenge = get_db().execute(
-        'SELECT id, title, body, created, thumbsup, score'
+    challenge = db.execute(
+        'SELECT id, flag, score'
         ' FROM challenge'
         ' WHERE id = ?',
         (id,)
     ).fetchone()
+    error = None
 
     if challenge is None:
-        abort(404, "Challenge id {0} doesn't exist.".format(id))
+        error = f'There is no challenge number {id}!'
 
-    if check_admin and g.user['isAdmin'] == False:
-        abort(403)
+    if error is not None:
+        flash(error)
+        return redirect(url_for('challenge.index'))
 
-    return challenge
+    flag = request.form['flag']
+
+    if not check_password_hash(challenge['flag'], flag):
+        flash(f'NOPE')
+        return redirect(url_for('challenge.index'))
+    
+    db.execute(
+        'UPDATE user'
+        ' SET score=?'
+        ' WHERE id = ?',
+        (int(challenge['score']), g.user['id'])
+    )
+    db.commit()
+    flash(f'CORRECT!')
+    return redirect(url_for('challenge.index'))
--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -6,7 +6,8 @@ CREATE TABLE user (
   isAdmin BOOL NOT NULL,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
-  email TEXT NOT NULL
+  email TEXT NOT NULL,
+  score INTEGER NOT NULL
 );
 CREATE TABLE challenge (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,28 +18,21 @@ CREATE TABLE challenge (
   flag TEXT NOT NULL,
   score INTEGER NOT NULL
 );
-CREATE TABLE record (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  timestamp TIMESTAMP NOT NULL,
-  solved BOOL NOT NULL,
-  user_id INTEGER NOT NULL,
-  challenge_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES user (id),
-  FOREIGN KEY (challenge_id) REFERENCES challenge (id)
-);
-INSERT INTO user (username, password, isAdmin, email)
+INSERT INTO user (username, password, isAdmin, email, score)
 VALUES
   (
     'test',
     'pbkdf2:sha256:150000$RFWE75Ir$76366beffd12322efb7eb52b913b671c969abfdfb91607d405129728b05e4838',
     0,
-    'maumau@maumau.com'
+    'maumau@maumau.com',
+    0
   ),
   (
     'lowte',
     'pbkdf2:sha256:150000$td2GOrSN$a17ce2468a922a26d07dddd7590d9a00dc33bb6a004035a74198c08623a9a576',
     1,
-    'lowte@gmail.com'
+    'lowte@gmail.com',
+    0
   );
 INSERT INTO challenge (title, body, thumbsup, flag, score)
 VALUES
